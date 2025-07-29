08368b28dcfd6d8097f6f68cb714cfd140168569
Stephen Kelly <gilgalad195@gmail.com>
2025-07-28 22:38:39 -0500

    added exit and help commands and registry

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		hasToken := scanner.Scan()
+		if hasToken {
+			lowerString := cleanInput(scanner.Text())
+			userInput := lowerString[0]
+			if command, exists := commands[userInput]; exists {
+				err := command.callback()
+				if err != nil {
+					fmt.Println("Error:", err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
+	}
 }
 
 func cleanInput(text string) []string {
 	cleanText := strings.Fields(strings.ToLower(text))
 	return cleanText
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
